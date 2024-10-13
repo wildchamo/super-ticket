97fe8e38a1eec6e8a86422f0b40a90d803047fec
Jose Luis <wildchamo@gmail.com>
2024-10-12 20:53:18 -0500

    connect form with javascript

--- components/login/login-form/index.jsx
+++ components/login/login-form/index.jsx
@@ -5,61 +5,73 @@ import { useRef } from "react";
 export const LoginForm = ({ isPasswordLogin }) => {
   const emailInputRef = useRef(null);
   const passwordInputRef = useRef(null);
+
+  const submitForm = async (event) => {
+    event.preventDefault();
+
+    if (isPasswordLogin) {
+      alert("password login");
+    } else {
+      alert("magic link login");
+    }
+  };
   return (
     <article style={{ maxWidth: "480px", margin: "auto" }}>
       <header>Login</header>
 
-      <fieldset>
-        <label htmlFor="email">
-          Email{" "}
-          <input
-            ref={emailInputRef}
-            type="email"
-            id="email"
-            name="email"
-            required
-          />
-        </label>
-
-        {isPasswordLogin && (
-          <label htmlFor="password">
-            Password{" "}
+      <form onSubmit={submitForm}>
+        <fieldset>
+          <label htmlFor="email">
+            Email{" "}
             <input
-              ref={passwordInputRef}
-              type="password"
-              id="password"
-              name="password"
+              ref={emailInputRef}
+              type="email"
+              id="email"
+              name="email"
+              required
             />
           </label>
-        )}
-      </fieldset>
 
-      <p>
-        {isPasswordLogin ? (
-          <Link
-            href={{
-              pathname: "/login",
-              query: { magicLink: "yes" },
-            }}
-          >
-            Go to Magic Link Login
-          </Link>
-        ) : (
-          <Link
-            href={{
-              pathname: "/login",
-              query: { magicLink: "no" },
-            }}
-          >
-            Go to Password Login
-          </Link>
-        )}
-      </p>
+          {isPasswordLogin && (
+            <label htmlFor="password">
+              Password{" "}
+              <input
+                ref={passwordInputRef}
+                type="password"
+                id="password"
+                name="password"
+              />
+            </label>
+          )}
+        </fieldset>
 
-      <button type="submit">
-        Sign in with
-        {isPasswordLogin ? " Password" : " Magic Link"}
-      </button>
+        <p>
+          {isPasswordLogin ? (
+            <Link
+              href={{
+                pathname: "/login",
+                query: { magicLink: "yes" },
+              }}
+            >
+              Go to Magic Link Login
+            </Link>
+          ) : (
+            <Link
+              href={{
+                pathname: "/login",
+                query: { magicLink: "no" },
+              }}
+            >
+              Go to Password Login
+            </Link>
+          )}
+        </p>
+
+        <button type="submit">
+          Sign in with
+          {isPasswordLogin ? " Password" : " Magic Link"}
+        </button>
+      </form>
     </article>
   );
 };
